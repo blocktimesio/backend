d826a1650bbd515c38e603c6b5750fb68d77af72
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-10-30 19:00:25 +0100

    [post] fix: Fixed readonly fields

--- web/apps/post/admin.py
+++ web/apps/post/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin.utils import flatten_fieldsets
 from django.contrib.auth import get_user_model
 from .admin_filters import PostOwnerFilter
 from ..users.models import User as UserModel
@@ -9,8 +10,6 @@ User = get_user_model()  # type: UserModel
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',), }
-
     list_display = ['name', 'slug', 'author', 'created', 'modified']
     list_filter = ['author', 'created', 'modified']
     search_fields = ['name', 'slug']
@@ -18,12 +17,34 @@ class TagAdmin(admin.ModelAdmin):
         ('Main', {'fields': ('name', 'slug')}),
     )
 
+    def get_prepopulated_fields(self, request, obj=None):
+        prepopulated_fields = {'slug': ('name',), }
+
+        if not obj:
+            return prepopulated_fields
+
+        is_author = obj.author == request.user
+        if not request.user.is_editor and not is_author:
+            return {}
+
+        return prepopulated_fields
+
+    def get_readonly_fields(self, request, obj=None):
+        if not obj:
+            return []
+
+        fields = ['name', 'slug']
+        is_author = obj.author == request.user
+        if not request.user.is_editor and not is_author:
+            return fields
+        return []
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'author':
             kwargs['queryset'] = User.objects.filter(type__in=User.TYPE.EDITORS)
         return super(TagAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-    def has_change_permission(self, request, obj=None):
+    def has_delete_permission(self, request, obj=None):
         if request.user.is_editor:
             return True
         if obj:
@@ -39,15 +60,40 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     filter_horizontal = ['tags']
-    prepopulated_fields = {'slug': ('name',), }
 
     list_display = ['name', 'status', 'locked', 'author', 'assigned', 'created', 'modified']
     list_filter = [PostOwnerFilter, 'status', 'locked', 'author', 'assigned', 'created', 'modified']
 
     fieldsets = (
-        ('Main', {'fields': (('name', 'slug'), 'pub_date')}),
-        ('Content', {'fields': ('content', 'tags')}),
+        ('Main', {'fields': ['name', 'slug']}),
+        ('Content', {'fields': ['content', 'tags']}),
     )
+
+    def get_prepopulated_fields(self, request, obj=None):
+        prepopulated_fields = {'slug': ('name',), }
+
+        if not obj:
+            return prepopulated_fields
+
+        is_author = obj.author == request.user
+        if obj.locked and not is_author:
+            return {}
+        if not request.user.is_editor and not is_author:
+            return {}
+
+        return prepopulated_fields
+
+    def get_readonly_fields(self, request, obj: Post=None):
+        if not obj:
+            return []
+
+        fields = flatten_fieldsets(self.fieldsets)
+        is_author = obj.author == request.user
+        if obj.locked and not is_author:
+            return fields
+        if not request.user.is_editor and not is_author:
+            return fields
+        return []
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'author':
