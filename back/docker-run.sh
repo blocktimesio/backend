c0316b0a4c1e64315459c171a243ffdf3c5df759
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-11-03 23:12:52 +0100

    [docker] chore: Loaded fixtres at run

--- back/docker-run.sh
+++ back/docker-run.sh
@@ -4,5 +4,6 @@ sleep 10s
 
 ./manage.py migrate
 ./manage.py collectstatic --noinput
+./manage.py loaddata project/fixtures/*.json
 
 gunicorn project.wsgi:application -w 2 --reload -b=unix:/app/run/gunicorn.socket
