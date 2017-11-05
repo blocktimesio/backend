ed6eadbf988c6b350a48b496ff47607efcdc91ed
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-11-05 17:36:42 +0100

    [crawlers] refact: Used base feed spider for CoinTelegraphFeedSpider

--- crawlers/blocktimes/spiders/cointelegraph_feed.py
+++ crawlers/blocktimes/spiders/cointelegraph_feed.py
@@ -1,12 +1,12 @@
-import os
-import scrapy
-import feedparser
+from scrapy.http import HtmlResponse
+from ..base_spiders import BaseFeedSpider
 from ..items import CoinTelegraphItem
-from dateutil.parser import *
 
 
-class CoinTelegraphFeedSpider(scrapy.Spider):
+class CoinTelegraphFeedSpider(BaseFeedSpider):
+    item = CoinTelegraphItem
     name = 'cointelegraph_feed'
+    slug_level = -1
     start_urls = ['https://cointelegraph.com/rss']
     custom_settings = {
         'ITEM_PIPELINES': {
@@ -15,34 +15,15 @@ class CoinTelegraphFeedSpider(scrapy.Spider):
         },
     }
 
-    def parse(self, response):
-        """ Load feed """
-        feed = feedparser.parse(response.body.decode())
-        for entry in feed.entries:
-            yield scrapy.Request(entry['link'], self.parse_article, meta={'entry': entry})
-
-    def parse_article(self, response):
-        entry = response.meta['entry']
-
+    def get_text(self, entry: dict, response: HtmlResponse) -> str:
         text = ''.join(
             [e.root for e in response.xpath('//div[@class="post-full-text contents"]//p//text()')]
         )
+        return text
 
-        slug = response.url.split('/')[-1]
-
-        file_name = os.path.basename(entry['media_content'][0]['url'])
-        image_name = slug + os.path.splitext(file_name)[1]
-        image_file_path = os.path.join(self.name, image_name)
-
-        yield CoinTelegraphItem(
-            url=response.url,
-            slug=slug,
-
-            title=entry['title'],
-            author=entry['author'],
-            text=text,
-            tags=','.join([t['term'] for t in entry['tags']]),
-            pub_date=str(parse(entry['published'])),
-            image_url=entry['media_content'][0]['url'],
-            image_file_path=image_file_path,
-        )
+    def get_image_url(self, entry: dict, response: HtmlResponse):
+        media_content = entry.get('media_content', [])
+        if len(media_content):
+            return media_content[0]['url']
+        else:
+            return ''
