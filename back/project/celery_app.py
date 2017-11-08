a0e83b1983de73576c65386fcb403dd536226cbe
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-11-08 20:46:30 +0100

    [news] refact: Moved social task to news app

--- back/project/celery_app.py
+++ back/project/celery_app.py
@@ -1,9 +1,8 @@
 import os
 from celery import Celery
-from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 app = Celery('blocktimes')
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(packages=['post'])
+app.autodiscover_tasks(packages=['news'])
