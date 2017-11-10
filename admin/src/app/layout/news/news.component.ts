a9cd55097373a27bcd107540e4e0e67f01a5e328
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-11-10 23:43:53 +0100

    [admin] feature: Made pagination for news list

--- admin/src/app/layout/news/news.component.ts
+++ admin/src/app/layout/news/news.component.ts
@@ -11,12 +11,29 @@ import { Http, Response } from '@angular/http';
 export class NewsComponent implements OnInit {
     public newsList: Array<any> = [];
 
+    public pageSize: Number = 10;
+    public totalCount: Number = 0;
+    public currentPage: Number = 1;
+
     constructor(private http: Http) {}
 
+    public changePage(selectedPage: number): void {
+        this.currentPage = selectedPage;
+        this.loadNews();
+    }
+
     ngOnInit() {
-        this.http.get('/api/v1/admin/news')
-            .subscribe((data: Response) => {
-                this.newsList = data.json().results;
+        this.loadNews();
+    }
+
+    private loadNews(): void {
+        // /api/v1/news/?limit=10&offset=50
+        const url = `/api/v1/admin/news?limit=${this.pageSize}&offset=${this.pageSize as number * this.currentPage as number}`;
+        this.http.get(url)
+            .subscribe((response: Response) => {
+                const data = response.json();
+                this.newsList = data['results'];
+                this.totalCount = data['count'] - 1;
             });
     }
 }
--- admin/src/app/layout/news/news.module.ts
+++ admin/src/app/layout/news/news.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 import { NewsComponent } from './news.component';
 import { NewsRoutingModule } from './news-routing.module';
 import { PageHeaderModule } from './../../shared';
+import { NgbAlertConfig, NgbPaginationConfig, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     imports: [
         CommonModule,
         NewsRoutingModule,
-        PageHeaderModule
+        PageHeaderModule,
+        NgbModule,
     ],
-    declarations: [NewsComponent]
+    declarations: [NewsComponent],
+    providers: [NgbAlertConfig, NgbPaginationConfig]
 })
 export class NewsModule { }
