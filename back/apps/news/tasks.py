66e196ea88756b2a2517d191bf54814e9b73dab7
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-11-11 13:31:47 +0100

    [back] refact: Removed redundant imports from tasks

--- back/apps/news/tasks.py
+++ back/apps/news/tasks.py
@@ -1,14 +1,8 @@
-import os
 import logging
 from datetime import timedelta
 from celery.task import periodic_task
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
-
-logger = logging.getLogger('crawlers')
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
 from .utils import SocialCrawler
 from crawlers.spiders.bitcoinist_feed import BitcoinistFeedSpider
 from crawlers.spiders.bitnewstoday import BitNewsTodaySpider
@@ -22,6 +16,8 @@ from crawlers.spiders.news_bitcoin_feed import NewsBitcoinFeedSpider
 from crawlers.spiders.newsbtc_feed import NewsBtcFeedSpider
 from crawlers.spiders.prestonbyrne_feed import PrestonByrneFeedSpider
 from crawlers.spiders.trustnodes_feed import TrustNodesFeedSpider
+
+logger = logging.getLogger('crawlers')
 
 
 @periodic_task(run_every=timedelta(minutes=10))
