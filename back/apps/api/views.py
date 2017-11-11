b3c23c2deef83f803f5977b1b10abf6c3a1f629b
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-11-11 23:51:41 +0100

    [back] feat: Made get News by ID

--- back/apps/api/views.py
+++ back/apps/api/views.py
@@ -1,11 +1,13 @@
 import logging
 
+from bson import ObjectId
 from django.conf import settings
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import APIException
-from rest_framework_mongoengine.viewsets import ModelViewSet as MongoModelViewSet
+from rest_framework_mongoengine.generics import get_object_or_404
+from rest_framework_mongoengine.viewsets import ModelViewSet as MongoModelViewSet, GenericViewSet
 from apps.api.permissions import IsSuperuser
 from .serializers import (NewsSerializer, RankConfigSerializer, SignInSerializer)
 from apps.news.models import (News, RankConfig)
@@ -22,6 +24,24 @@ class NewsViewSet(MongoModelViewSet):
 
     def get_queryset(self):
         return sorted(News.objects.all(), key=lambda n: n.rank, reverse=True)
+
+    def get_object(self):
+        queryset = self.filter_queryset(News.objects.all())
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+            'Expected view %s to be called with a URL keyword argument '
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            'attribute on the view correctly.' %
+            (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        if 'id' in filter_kwargs:
+            filter_kwargs['id'] = ObjectId(filter_kwargs['id'])
+        obj = get_object_or_404(queryset, **filter_kwargs)
+        self.check_object_permissions(self.request, obj)
+        return obj
 
 
 class RankConfigView(APIView):
