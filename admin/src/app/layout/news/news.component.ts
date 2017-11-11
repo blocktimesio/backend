d8c1c062306b0b74283b2e35ccefb220fc99e688
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-11-11 10:04:37 +0100

    [admin] feat: Save rank config form & update news list

--- admin/src/app/layout/news/news.component.ts
+++ admin/src/app/layout/news/news.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { Http, Response } from '@angular/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-tables',
@@ -8,32 +9,57 @@ import { Http, Response } from '@angular/http';
     styleUrls: ['./news.component.scss'],
     animations: [routerTransition()]
 })
-export class NewsComponent implements OnInit {
+export class NewsComponent {
     public newsList: Array<any> = [];
+
+    public rankConfigForm: FormGroup;
 
     public pageSize: Number = 10;
     public totalCount: Number = 0;
     public currentPage: Number = 1;
 
-    constructor(private http: Http) {}
+    constructor(private http: Http, private fb: FormBuilder) {
+        this.rankConfigForm = fb.group({
+            'fb_shares' : [null, Validators.required],
+            'linkedin_shares' : [null, Validators.required],
+            'reddit_up' : [null, Validators.required],
+            'twitter_shares' : [null, Validators.required],
+            'views' : [null, Validators.required],
+            'comments' : [null, Validators.required],
+            'date_elapsed_seconds' : [null, Validators.required],
+            'date_coef' : [null, Validators.required],
+        });
+        this.loadRankConfigForm();
+        this.loadNews();
+    }
 
     public changePage(selectedPage: number): void {
         this.currentPage = selectedPage;
         this.loadNews();
     }
 
-    ngOnInit() {
-        this.loadNews();
+    public submitRankConfig(): void {
+        const data = this.rankConfigForm.value;
+        this.http.post('/api/v1/admin/config/rank', data)
+            .subscribe((response: Response) => this.loadNews());
     }
 
     private loadNews(): void {
         // /api/v1/news/?limit=10&offset=50
-        const url = `/api/v1/admin/news?limit=${this.pageSize}&offset=${this.pageSize as number * this.currentPage as number}`;
+        const url = `/api/v1/admin/news?limit=${this.pageSize}&offset=${this.pageSize as number}`;
         this.http.get(url)
             .subscribe((response: Response) => {
                 const data = response.json();
                 this.newsList = data['results'];
                 this.totalCount = data['count'] - 1;
+            });
+    }
+
+    private loadRankConfigForm() {
+        this.http.get('/api/v1/admin/config/rank')
+            .subscribe((response: Response) => {
+                const data = response.json();
+                this.rankConfigForm.reset(data);
             });
     }
 }
--- admin/src/app/layout/news/news.module.ts
+++ admin/src/app/layout/news/news.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { NewsComponent } from './news.component';
 import { NewsRoutingModule } from './news-routing.module';
 import { PageHeaderModule } from './../../shared';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbAlertConfig, NgbPaginationConfig, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
         NewsRoutingModule,
         PageHeaderModule,
         NgbModule,
