b85b0a7af3771bd6c0019e5db000050714281b5f
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-11-12 14:18:33 +0100

    [back] chore: Created settings for tests

--- back/project/settings_tests.py
+++ back/project/settings_tests.py
@@ -1,37 +1,20 @@
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'root': {
-        'level': 'DEBUG',
-        'handlers': [],
-    },
-    'formatters': {
-        'verbose': {
-            'format': '[%(levelname)s] %(asctime)s - %(module)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['console', ],
-            'level': 'DEBUG',
-            'propagate': False
-        },
-        'django.db.backends': {
-            'level': 'WARNING',
-            'handlers': ['console', ],
-            'propagate': False,
-        },
-        'notification': {
-            'handlers': ['console', ],
-            'level': 'ERROR',
-            'propagate': False
-        },
-    },
-}
+from .settings import *
+
+AUTH_PASSWORD_VALIDATORS = []
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/tmp/emails/blocktimes'
+
+
+class DisableMigrations(object):
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return 'notmigrations'
+
+MIGRATION_MODULES = DisableMigrations()
