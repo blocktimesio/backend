b60f8eddc9c1d22749be93784daab06edd6f6b62
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-11-12 22:47:53 +0100

    [admin] feat: Added save & remove btns for news detail

--- admin/src/app/layout/news/components/news-detail/news-detail.component.ts
+++ admin/src/app/layout/news/components/news-detail/news-detail.component.ts
@@ -12,6 +12,7 @@ import { routerTransition } from '../../../../router.animations';
 })
 export class NewsDetailComponent {
     public news: any = null;
+    public alerts: Array<any> = [];
 
     constructor(private http: Http,
                 private router: Router,
@@ -32,4 +33,40 @@ export class NewsDetailComponent {
         imageMaxSize: 30 * 1024 * 1024,
         imageAllowedTypes: ['jpeg', 'jpg', 'png'],
     };
+
+    public saveNews(): void {
+        this.route.params.subscribe(params => {
+            this.http.patch(`/api/v1/admin/news/${params['id']}/`, this.news)
+                .subscribe((data: Response) => {
+                    this.alerts.push({
+                        type: 'success',
+                        message: 'Saving news was succeed',
+                    });
+                }, (error) => {
+                    this.alerts.push({
+                        type: 'danger',
+                        message: 'Saving news was failure',
+                    });
+                });
+        });
+    }
+
+    public removeNews(): void {
+        this.route.params.subscribe(params => {
+            this.http.delete(`/api/v1/admin/news/${params['id']}/`)
+                .subscribe((data: Response) => {
+                    this.router.navigate(['/news']);
+                }, (error) => {
+                    this.alerts.push({
+                        type: 'danger',
+                        message: 'Removing news was failure',
+                    });
+                });
+        });
+    }
+
+    public closeAlert(alert: any) {
+        const index: number = this.alerts.indexOf(alert);
+        this.alerts.splice(index, 1);
+    }
 }
