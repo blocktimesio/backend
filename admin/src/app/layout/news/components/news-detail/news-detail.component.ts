035e2b96fb47dc6f056017ef6e9d114da7e449f2
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-11-12 23:28:06 +0100

    [admin] chore: Decrease time

--- admin/src/app/layout/news/components/news-detail/news-detail.component.ts
+++ admin/src/app/layout/news/components/news-detail/news-detail.component.ts
@@ -27,7 +27,7 @@ export class NewsDetailComponent {
                 });
         });
 
-        setTimeout(() => $('.fr-wrapper > div > a').remove(), 2300);
+        setTimeout(() => $('.fr-wrapper > div > a').remove(), 1000);
     }
 
     public editorEdit = {
