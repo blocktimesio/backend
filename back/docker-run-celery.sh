5cb2c1e63430a3e62d34e4b5f4e69244ad10ac4e
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-11-26 18:13:58 +0100

    [docker] refact: Merged celery & back containers

--- back/docker-run-celery.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sleep 10s
-
-celery -A project worker --beat --loglevel=info \
-                        --pidfile="run/celery.pid" \
-                        --logfile="logs/celery.log" \
-                        --schedule="run/celerybeat-schedule"
--- back/docker-run.sh
+++ back/docker-run.sh
@@ -6,6 +6,11 @@ sleep 10s
 ./manage.py collectstatic --noinput
 ./manage.py loaddata project/fixtures/*.json
 
+celery -A project worker --beat --loglevel=info \
+                        --pidfile="run/celery.pid" \
+                        --logfile="logs/celery.log" \
+                        --schedule="run/celerybeat-schedule" &
+
 gunicorn project.wsgi:application -w 2 --log-level=info \
             --reload -b=unix:/app/run/gunicorn.socket \
             --error-logfile=/app/logs/gunicorn-error.log \
