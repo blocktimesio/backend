ac9c32eed115f0e30c8c1a42500612fe401a5ec4
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-11-26 19:34:27 +0100

    [back] refact: Moved load image & update_social_data to News model

--- back/apps/news/crawlers/pipelines.py
+++ back/apps/news/crawlers/pipelines.py
@@ -1,10 +1,5 @@
-import os
 import logging
-import requests
-import socialshares
-from django.core.files import File
-from django.core.files.temp import NamedTemporaryFile
-from ..models import (Tag, Domain, News)
+from ..models import (Domain, News)
 
 logger = logging.getLogger('crawlers')
 
@@ -12,68 +7,21 @@ logger = logging.getLogger('crawlers')
 class DjangoPipeline(object):
     def process_item(self, item, spider):
         tags_names = item.pop('tags', [])
-        tags = []
-        for name in tags_names:
-            tag, created = Tag.objects.get_or_create(name=name)
-            tags.append(tag)
 
         domain_name = item.pop('domain')
         domain, created = Domain.objects.get_or_create(name=domain_name)
 
-        social_data = {}
-        try:
-            social_data = socialshares.fetch(
-                item['url_raw'],
-                ['pinterest', 'linkedin', 'google', 'reddit']
-            )
-            fb_data = self._get_fb_data(item['url_raw'])
-            social_data.update(fb_data)
-
-        except Exception as e:
-            logger.error('Err at get social data', exc_info=True)
-
         is_exists = News.objects.filter(url=item['url']).exists()
         if is_exists:
             try:
-                News.objects.filter(url=item['url']).update(social=social_data, **dict(item))
+                News.objects.filter(url=item['url']).update(
+                    views=item['views'],
+                    comments=item['comments'],
+                )
             except Exception as e:
                 logger.error('Error at update News', exc_info=True)
         else:
-            obj = News.objects.create(domain=domain, social=social_data, **item)
-            for tag in tags:
-                obj.tags.add(tag)
+            obj = News.objects.create(domain=domain, social={}, **item)
 
-            image_url = item.get('image_url')
-            if image_url:
-                try:
-                    res = requests.get(image_url)
-                    if res.status_code == requests.codes.ok:
-                        img_temp = NamedTemporaryFile(delete=True)
-                        img_temp.write(res.content)
-                        img_temp.flush()
-
-                        image_path = 'news_images/{}/{}'.format(
-                            obj.domain.name,
-                            os.path.basename(image_url)
-                        )
-                        obj.image.save(image_path, File(img_temp), save=True)
-                except Exception as e:
-                    logger.error('Error at save image for news', exc_info=True)
-            try:
-                obj.save()
-            except Exception as e:
-                logger.error('Error at create new News', exc_info=True)
-
-    def _get_fb_data(self, url: str) -> dict:
-        fb_url = 'https://graph.facebook.com/?key={}&id={}'.format(
-            os.environ.get('FB_KEY', ''),
-            url
-        )
-        fb_response = requests.get(fb_url)
-        if fb_response.status_code == 200:
-            fb_data = fb_response.json()
-            share_data = fb_data.get('share', {})
-            return {'facebook': share_data}
-        else:
-            logger.warning('Can\'t get FB data for {}'.format(url))
-            return {}
+            obj.load_image()
+            obj.add_tags(tags_names)
--- back/apps/news/models.py
+++ back/apps/news/models.py
@@ -1,11 +1,19 @@
+import os
+import logging
+import requests
+import socialshares
 from django.conf import settings
+from django.core.files import File
 from django.contrib.postgres.fields import JSONField
+from django.core.files.temp import NamedTemporaryFile
 from django.db import models
 from datetime import datetime
 from django.template.defaultfilters import (
     striptags, truncatewords, truncatechars
 )
 from solo.models import SingletonModel
+
+logger_crawlers = logging.getLogger('crawlers')
 
 
 class RankConfig(SingletonModel):
@@ -105,6 +113,87 @@ class News(models.Model):
     @property
     def short_text(self):
         return truncatewords(striptags(self.text), 30)
+
+    def update_social_data(self) -> None:
+        if not self.url_raw:
+            message = 'News ID={} has not url_raw field'.format(self.id)
+            logger_crawlers.warning(message)
+            return
+
+        social_data = {}
+        try:
+            social_data = socialshares.fetch(
+                self.url_raw,
+                ['pinterest', 'linkedin', 'google', 'reddit']
+            )
+            fb_data = self._get_fb_data(self.url_raw)
+            social_data.update(fb_data)
+        except Exception as e:
+            message = 'Error at fetch social shares for {}'.format(self.url_raw)
+            logger_crawlers.error(message, exc_info=True)
+
+        self.social = social_data
+        try:
+            self.save()
+        except Exception as e:
+            message = 'Error at saving News ID={} at updating social data'.format(self.id)
+            logger_crawlers.error(message)
+
+    def _get_fb_data(self, url: str) -> dict:
+        if not self.url_raw:
+            message = 'News ID={} has not url_raw field'.format(self.id)
+            logger_crawlers.warning(message)
+            return {}
+
+        fb_url = 'https://graph.facebook.com/?key={}&id={}'.format(
+            os.environ.get('FB_KEY', ''),
+            url
+        )
+        fb_response = requests.get(fb_url)
+        if fb_response.status_code == 200:
+            fb_data = fb_response.json()
+            share_data = fb_data.get('share', {})
+            return {'facebook': share_data}
+        else:
+            message = 'Error at get Facebook shares for {}'.format(self.url_raw)
+            logger_crawlers.error(message, exc_info=True)
+            return {}
+
+    def load_image(self) -> None:
+        if not self.image_url:
+            message = 'News ID={} has not field image_url'
+            logger_crawlers.warning(message)
+            return
+
+        try:
+            res = requests.get(self.image_url)
+            if res.status_code == requests.codes.ok:
+                img_temp = NamedTemporaryFile(delete=True)
+                img_temp.write(res.content)
+                img_temp.flush()
+
+                image_path = 'news_images/{}/{}'.format(
+                    self.domain.name,
+                    os.path.basename(self.image_url)
+                )
+                self.image.save(image_path, File(img_temp), save=True)
+        except Exception as e:
+            message = 'Error at saving image for News ID={}'.format(self.id)
+            logger_crawlers.error(message, exc_info=True)
+
+        try:
+            self.save()
+        except Exception as e:
+            logger_crawlers.error('Error at create News', exc_info=True)
+
+    def add_tags(self, tags_names: list) -> None:
+        tags = []
+        for name in tags_names:
+            tag, created = Tag.objects.get_or_create(name=name)
+            tags.append(tag)
+
+        for tag in tags:
+            self.tags.add(tag)
 
     def __str__(self):
         return truncatechars(self.title, 32)
