9978ad8830f729877a49852e1989b83e8637f73a
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-12-06 00:37:26 +0100

    [back] fix: Fixed pub API urls

--- back/apps/api/publick/urls.py
+++ back/apps/api/publick/urls.py
@@ -1,17 +1,11 @@
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
-from rest_framework_jwt.views import obtain_jwt_token
 from .views import (
-    DomainViewSet, TagViewSet, NewsViewSet,
-    RankConfigView, SignInView, load_image,
-    FlatpageViewSet
+    NewsViewSet,
 )
 
 router = DefaultRouter()
-router.register(r'tag', TagViewSet, r'news')
-router.register(r'domain', DomainViewSet, r'news')
 router.register(r'news', NewsViewSet, r'news')
-router.register(r'flatpage', FlatpageViewSet, r'flatpage')
 
 urlpatterns = [
     url(r'', include(router.urls)),
