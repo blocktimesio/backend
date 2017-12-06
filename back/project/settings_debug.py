8145acd151535d28b9a52702445645ed896033e9
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-12-06 18:05:51 +0100

    [back] chore: Created settings_debug.py

---
+++ back/project/settings_debug.py
@@ -0,0 +1,10 @@
+from .settings import *
+
+AUTH_PASSWORD_VALIDATORS = []
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/tmp/emails/blocktimes'
